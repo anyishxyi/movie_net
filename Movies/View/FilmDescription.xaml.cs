19d7267f0e9af3f88b2f3d96431885a8151adc80
Sylvain <rey_s@etna-alternance.net>
2019-07-15 15:30:14 +0200

    Amélioration de la fiche des films

--- Movies/View/FilmDescription.xaml.cs
+++ Movies/View/FilmDescription.xaml.cs
@@ -27,5 +27,10 @@ namespace Movies
             if (vm.CloseAction == null)
                 vm.CloseAction = new Action(this.Close);
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
