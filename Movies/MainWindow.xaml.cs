cdacb69abcc186567793242569c405fc8377fd8c
Sylvain <rey_s@etna-alternance.net>
2019-07-15 18:18:00 +0200

    Traitement des données à réaliser

--- Movies/MainWindow.xaml.cs
+++ Movies/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Data;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Movies
 {
@@ -15,6 +17,28 @@ namespace Movies
             this.DataContext = vm;
             if (vm.CloseAction == null)
                 vm.CloseAction = new Action(this.Close);
+        }
+
+        // filtrage des donnees
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            listbox1.SelectedItems.Clear();
+
+            for (int i = listbox1.Items.Count - 1; i >= 0; i--)
+            {
+                if (listbox1.Items[i].ToString().ToLower().Contains(txt_search.Text.ToLower()))
+                {
+                }
+                
+            }
+ 
+        }
+
+        // Si un item est selectionne on peut agir dessus par la suite
+        // Faire attention au code behind une fois l'implementation reussie
+        private void Listbox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
